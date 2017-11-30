c053b42c3da2943102b53ddd84ca50f846b8d24e
sor3nt@gmail.com <hyp3r$1daewoo>
2017-11-30 19:41:13 +0100

    fix install

--- install.php
+++ install.php
@@ -8,10 +8,16 @@
 
     echo "Copy files to /opt/cleaner\n";
     copy('cleaner.php', '/opt/cleaner/cleaner.php');
+
+    copy('src/CompareFileNames.php', '/opt/cleaner/src/CompareFileNames.php');
+    copy('src/DuplicateMatcher.php', '/opt/cleaner/src/DuplicateMatcher.php');
+    copy('src/Emulator.php', '/opt/cleaner/src/Emulator.php');
+    copy('src/Emulators.php', '/opt/cleaner/src/Emulators.php');
     copy('src/GameEntry.php', '/opt/cleaner/src/GameEntry.php');
     copy('src/GameList.php', '/opt/cleaner/src/GameList.php');
-    copy('src/VideoMatcher.php', '/opt/cleaner/src/VideoMatcher.php');
-    copy('src/DuplicateMatcher.php', '/opt/cleaner/src/DuplicateMatcher.php');
+    copy('src/Helper.php', '/opt/cleaner/src/Helper.php');
+    copy('src/Log.php', '/opt/cleaner/src/Log.php');
+    copy('src/Matcher.php', '/opt/cleaner/src/Matcher.php');
 
     if (!file_exists('/usr/bin/cleaner')){
         echo "Create executabele /usr/bin/cleaner\n";
